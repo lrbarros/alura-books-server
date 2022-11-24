d06e527c2a29ac8390ec685d8fc75f105c93d2bf
bernardosevero <bernardoseverosilveira@gmail.com>
2022-11-24 03:06:45 -0300

    aula 5

--- controllers/livros.js
+++ controllers/livros.js
@@ -1,58 +1,71 @@
-const livros = require("../livros.json")
-const fs = require("fs")
 const { getTodosLivros, getLivro, modificaLivro, insereLivro, deletaLivro } = require("../servicos/livros")
 
 function getLivros(req,res) {
     try {
         const livros = getTodosLivros()
-        res.json(livros)
+        res.send(livros)
     } catch (error) {
         res.status(500)
-        res.json({error})   
+        res.send({mensagem: error.message})   
     }
 }
 
 function getLivroPorId(req,res) {
     try {
         const id = req.params.id
-        const livros = getLivro(id)
-        console.log(livros)
-        res.json(livros)
+        if (id && Number(id)) {
+            const livros = getLivro(id)
+            res.send(livros)
+        }
+        
+        res.status(422)
+        res.send({message:"Id inválido"})
     } catch (error) {
         res.status(500)
-        res.json({error})   
+        res.send({mensagem: error.message})   
     }
 }
 
 function postLivro(req, res) {
     try {
-        const livroInserido = insereLivro(req.body)
-        res.status(201)
-        res.json(livroInserido)
+        if(req.body.name) {
+            const livroInserido = insereLivro(req.body)
+            res.status(201)
+            res.send({livroInserido})
+        }
+
+        res.status(422)
+        res.send({mensagem: "O campo name é obrigatório"})
+        
     } catch (error) {
         res.status(500)
-        res.json({error})
+        res.send({mensagem: error.message})   
     }
 }
 
 function patchLivro(req, res) {
     try {
-        const livros = modificaLivro(req.params.id, req.body)
+        const id = req.params.id
+        if (id && Number(id)) {
+            const livroMudado = modificaLivro(req.params.id, req.body)
+            res.send({livroMudado})   
+        }
 
-        res.json(livros)
+        res.status(422)
+        res.send({message:"Id inválido"})
     } catch (error) {
         res.status(500)
-        res.json({error})
+        res.send({mensagem: error.message})   
     }
 }
 
 function deleteLivro(req, res) {
     try {
         const idDeletado = deletaLivro(req.params.id)
-        res.json({idDeletado})
+        res.send({idDeletado})
     } catch (error) {
         res.status(500)
-        res.json({error})
+        res.send({mensagem: error.message})   
     }
 }
 
--- servicos/livros.js
+++ servicos/livros.js
@@ -11,17 +11,22 @@ function getLivro(id) {
 function modificaLivro(id, conteudo) {
     let livrosAtuais = JSON.parse(fs.readFileSync("livros.json"))
     let indiceLivro = livrosAtuais.findIndex(livro => livro.id === id)
+    if(indiceLivro === -1 ) {
+        throw new Error("id inexistente")
+    }
     const conteudoMudado = {...livrosAtuais[indiceLivro], ...conteudo}
     livrosAtuais[indiceLivro] = conteudoMudado
 
     fs.writeFileSync("livros.json",JSON.stringify(livrosAtuais))
 
-    return livrosAtuais
+    return conteudo
 }
 
 function insereLivro(novoLivro) {
     const livrosAtuais = JSON.parse(fs.readFileSync("livros.json"))
     fs.writeFileSync("livros.json",JSON.stringify([...livrosAtuais, novoLivro]))
+
+    return novoLivro
 }
 
 function deletaLivro(id) {
