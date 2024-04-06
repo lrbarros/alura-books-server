379d0a44a135cb17373bd0e5072ab4373c75d500
luiz <luizricardobarros@gmail.com>
2024-04-05 21:37:50 -0300

    server para teste

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const routerLivros = require('./rotas/livros')
+const rotaFavorito = require("./rotas/favorito")
+const cors = require('cors');
+
 const app = express()
+app.use(cors());
+
 const port = 8000
 
 app.use(express.json())
-app.use('/livros', routerLivros)
 
+app.use('/livros', routerLivros)
+app.use('/favoritos', rotaFavorito)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
